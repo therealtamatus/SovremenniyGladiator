d5f01602af1842f10d5272c710ceb8b02573816f
sanoore <sanooresan@gmail.com>
2018-03-18 22:31:33 +0200

    Удаление лишних строк кода

--- IGORA/Assets/Scripts/buttons/newgame.cs
+++ IGORA/Assets/Scripts/buttons/newgame.cs
@@ -4,28 +4,13 @@ using UnityEngine;
 
 public class newgame : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	void OnMouseOver()
+    {
 	
-	// Update is called once per frame
-	void Update () {
-		
-		
-		}
-	void OnMouseOver(){
-	
-		if (Input.GetMouseButtonDown (0)) {
-
-			Debug.Log ("SOSKA");
+		if (Input.GetMouseButtonDown (0))
+        {
 			Application.LoadLevel("1level");
 		}
 	
 	}
-
-
-
-
-
 }
