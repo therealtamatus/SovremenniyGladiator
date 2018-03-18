405bbbe98e298e9a1d59761969b8d84bbebec84a
Tamatug <therealtamatus@gmail.com>
2018-03-18 23:11:21 +0300

    Начальные менюшки
    Добавил кароч кнопки в меню, а там открытие сцены первого уровня. Ничего особенного

---
+++ IGORA/Assets/Scripts/buttons/newgame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newgame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+		}
+	void OnMouseOver(){
+	
+		if (Input.GetMouseButtonDown (0)) {
+
+			Debug.Log ("SOSKA");
+			Application.LoadLevel("1level");
+		}
+	
+	}
+
+
+
+
+
+}
