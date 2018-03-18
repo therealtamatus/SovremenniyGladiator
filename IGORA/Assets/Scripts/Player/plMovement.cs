834b68623d4115cb97f445bc48dbf11d599436ca
sanoore <sanooresan@gmail.com>
2018-03-18 20:53:33 +0200

    Изменения в проекте.
    Добавлен джойстик из assetStore
    Создан скрипт plMovement:
     Передвижение с помощью джойстика.
    Создан скрипт plRotation(кажется)
     Поворот по направляению джойстика.

---
+++ IGORA/Assets/Scripts/Player/plMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plMovement : MonoBehaviour {
+
+    private CharacterController _charContr;
+    public float speed;
+    public Joystick joystick;
+
+	void Start ()
+    {
+        _charContr = GetComponent<CharacterController>();
+	}
+	
+	void Update ()
+    {
+        PlayerMovement();
+	}
+
+    void PlayerMovement()
+    {
+        //float deltaX = Input.GetAxis("Horizontal") * speed;
+        //float deltaY = Input.GetAxis("Vertical") * speed;
+        float deltaX = joystick.Horizontal* speed;
+        float deltaY = joystick.Vertical * speed;
+        Vector3 movement = new Vector3(deltaX, deltaY, 0);
+        movement = Vector3.ClampMagnitude(movement, speed);
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _charContr.Move(movement);
+    }
+}
---
+++ IGORA/Assets/Scripts/Player/plRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plRotation : MonoBehaviour
+{
+
+    public Joystick joystick;
+
+    void Update()
+    {
+        float angle= Mathf.Atan2(joystick.Horizontal, joystick.Vertical)*Mathf.Rad2Deg*-1f;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
