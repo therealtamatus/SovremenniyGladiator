2d99ee9d46a32d727c52e08502884b01feabe0bf
sanoore <sanooresan@gmail.com>
2018-03-19 10:18:03 +0200

    Изменения в камере
    В plRotation script добавлено позиционирование на обьект.

--- IGORA/Assets/Scripts/Player/plRotation.cs
+++ IGORA/Assets/Scripts/Player/plRotation.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class plRotation : MonoBehaviour
 {
-
+    public Transform target;
     public Joystick joystick;
+
+    public bool isTarget=false;
+    
 
     void Update()
     {
-        float angle= Mathf.Atan2(joystick.Horizontal, joystick.Vertical)*Mathf.Rad2Deg*-1f;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        if (isTarget)
+        {
+            Vector3 dir = target.position - transform.position;
+            float angle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg * -1f;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+        else
+        {
+            float angle = Mathf.Atan2(joystick.Horizontal, joystick.Vertical) * Mathf.Rad2Deg * -1f;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
     }
+    
 }
